5386f1cfec58e1475af49b9cae8aba592c587b1b
Rihan <me@file.properties>
2022-06-06 01:48:10 +0000

    Rename to basic header

---
+++ components/HeaderBasic.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  name: string;
+  model: string;
+}>();
+</script>
+
+<template>
+  <div class="text-center">
+    <Model
+      class="h-[300px] mb-3"
+      :model="model"
+      interaction-prompt="none"
+      camera-orbit="270deg 80deg 120%"
+      auto-rotate
+    />
+    <h1 class="font-bold text-2xl">{{ name }}</h1>
+  </div>
+</template>
--- components/SnailCard.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div
-    class="rounded-lg bg-gray-200 hover:bg-gray-300 border-gray-300 hover:border-gray-400 border-2 transition-all cursor-pointer"
-  >
-    <Model class="" model="/model/scene.gltf" />
-  </div>
-</template>
