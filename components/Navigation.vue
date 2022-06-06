f3327f8890b4b8587c0d47bb63454ad1f8ece470
Rihan <me@file.properties>
2022-06-06 01:48:30 +0000

    Remove nav temporarily

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,6 +1,1 @@
-<template>
-  <div class="bg-gray-900 rounded-br-lg rounded-bl-lg py-3 px-2 mb-5">
-    <h1 class="font-medium text-gray-300">Den Den Mushi</h1>
-    <h2 class="font-bold text-xl text-white">Home</h2>
-  </div>
-</template>
+<template></template>
